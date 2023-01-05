4a98d4dfaeb8914c5a9cde8fdb904970fec4d20e
Hema <mshemadarshini@gmail.com>
2023-01-05 16:20:34 +0800

    update info

---
+++ schemas.py
@@ -0,0 +1,39 @@
+from typing import List, Union
+from pydantic import BaseModel
+from pydantic import BaseModel
+
+
+class ItemBase(BaseModel):
+    title: str
+    # if not description, then default None value added
+    description: Union[str, None] = None
+
+
+class ItemCreate(ItemBase):
+    pass
+
+
+class Item(ItemBase):
+    id: int
+    owner_id: int
+
+    class Config:
+        orm_mode = True
+
+class UserBase(BaseModel):
+    username: str
+    is_active: bool
+    email: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+    is_active: bool
+    items: List[Item] = []
+
+    class Config:
+        orm_mode = True
